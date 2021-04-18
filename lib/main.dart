a424ac6b21cf07f23cea72a9cf190871e46ab174
kadweka <husseinkatana17@gmail.com>
2021-04-19 02:34:08 +0300

    intro to const variable in dart and its restruc

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,35 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'Whats your Favourite color',
+      'answers': ['black', 'White', 'Red', 'Green']
+    },
+    {
+      'questionText': 'Whats your Favourite Animal',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+    },
+    {
+      'questionText': 'Whats your Favourite Instructor',
+      'answers': ['Hussein', 'Kadweka', 'Katana', 'Max']
+    },
+  ];
   var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    if (_questionIndex < questions.length) {
+      print('We have more Questions');
+    }
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'Whats your favourite color?',
-      'Whats your favouritr animal?',
-    ];
+    // questions = []; does not work of questions is a const
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -38,10 +52,11 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            Answer(answerQuestion),
-            Answer(answerQuestion),
-            Answer(answerQuestion),
+            Question(questions[_questionIndex]['questionText']),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
